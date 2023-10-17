0959e9f452191439e21c0b77072db48de5e7b958
Arbi <arb.rugova43@gmail.com>
2023-10-17 23:46:07 +0200

    code

--- src/components/Works.jsx
+++ src/components/Works.jsx
@@ -28,7 +28,7 @@ const ProjectCard = ({
           scale: 1,
           speed: 1000,
         }}
-        className="bg-tertiary p-5 rounded-2xl sm:w-[360px] w-full"
+        className="bg-tertiary p-5 rounded-2xl sm:w-[360px] w-full cursor-pointer"
       >
         <div className="relative w-full h-[230px]">
           <img
